12639e87720be61fcd2625ed0f3e499e0334fa6a
butterr12 <126962777+butterr12@users.noreply.github.com>
2025-03-01 05:34:23 +0800

    Updates

---
+++ _app/immutable/chunks/NU866roM.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1udgk1d)==null?void 0:s.base)??"/honesty-store";var e;const a=((e=globalThis.__sveltekit_1udgk1d)==null?void 0:e.assets)??t;export{a,t as b};
