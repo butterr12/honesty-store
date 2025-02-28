b5211fc1483360b640200462e445528c5643f002
butterr12 <126962777+butterr12@users.noreply.github.com>
2025-03-01 05:46:13 +0800

    Updates

---
+++ _app/immutable/chunks/DNZZGJ9I.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_l8yol2)==null?void 0:s.base)??"/honesty-store";var e;const l=((e=globalThis.__sveltekit_l8yol2)==null?void 0:e.assets)??t;export{l as a,t as b};
