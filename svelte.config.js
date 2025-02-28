f23ee424bbad52ef7cecb7c25d3ec9e6e2797a1c
butterr12 <126962777+butterr12@users.noreply.github.com>
2025-03-01 05:05:37 +0800

    Copied files from gh-pages to main

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
-  kit: {
-    adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/honesty-store' : '',
-    },
-    appDir: 'app',
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/honesty-store' : ''
+		},
+		prerender: {
+			entries: ["*"]
+		}
+	}
 };
+
+export default config;
