dbd7886a0ee8195c2af5a6f48b5ffb809d81cd3b
butterr12 <126962777+butterr12@users.noreply.github.com>
2025-03-01 04:06:47 +0800

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: '/honesty-store' 
+    }
+  }
 };
 
 export default config;
