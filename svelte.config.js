a7c0b6ef32ee7f1487e52d83e6c9bd14819b669a
butterr12 <126962777+butterr12@users.noreply.github.com>
2025-03-01 03:39:37 +0800

    svelte config modify

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+export default {
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: "/honesty-store", 
+      assets: "/honesty-store"
+    },
+    prerender: {
+      default: true
+    }
+  }
 };
-
-export default config;
